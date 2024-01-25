782b6ab5a2e9c85ae2c6de7da88ab510c405a9a8
shinheylynn <heylub@gmail.com>
2024-01-25 21:14:06 +0900

    :lipstick: Add: 폰트 변경 select-box 기능 추

--- src/keyboard.js
+++ src/keyboard.js
@@ -27,7 +27,6 @@ function handleKeyPress(event) {
     const key = document.querySelector(`.key[data-code="Key${event.key.toUpperCase()}"]`);
     if (key) {
       key.style.backgroundColor = 'black';
-      updateInput(key.getAttribute('data-val'));
     }
   }
 }
@@ -49,3 +48,38 @@ document.querySelector('.mode-switch-button button').addEventListener('click', f
   this.classList.toggle('active');
   document.body.classList.toggle('dark-mode');
 })
+
+// font select-box
+
+document.addEventListener('DOMContentLoaded', function() {
+  const selectBox = document.querySelector('.font__select-box');
+  const selectedFontSpan = document.querySelector('.font__selected-font');
+
+  // 선택 상자 클릭 이벤트
+  document.querySelector('.font__selected-font').addEventListener('click', function() {
+      selectBox.classList.toggle('active');
+  });
+
+  // 리스트 항목 클릭 이벤트
+  selectBox.querySelectorAll('li').forEach(item => {
+      item.addEventListener('click', function() {
+          const fontValue = this.getAttribute('data-value');
+          const fontText = this.textContent;
+
+          if (document.body.className.includes('dark-mode')) {
+            document.body.className = 'dark-mode ' + fontValue;
+          } else { document.body.className = fontValue };
+
+          selectedFontSpan.textContent = fontText; // .font__selected-font 요소의 텍스트 변경
+          
+          selectBox.classList.remove('active'); // 리스트 숨김
+      });
+  });
+
+  // 외부 클릭 감지하여 리스트 숨기기
+  document.addEventListener('click', function(event) {
+      if (!selectBox.contains(event.target) && !event.target.matches('.font__selected-font')) {
+          selectBox.classList.remove('active');
+      }
+  });
+});
